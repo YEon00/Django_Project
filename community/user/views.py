d9556dd6676a0e3c04aac3d132811bb1b29b666f
YEon00 <danbicyy@naver.com>
2020-12-10 14:44:38 +0900

    add user register fucntion

--- community/user/views.py
+++ community/user/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
+#패스워드 암호화 하는 라이브러리
 from .models import user
 # Create your views here.
 
@@ -7,13 +10,22 @@ def register(request):
     if request.method == 'GET':
         return render(request,'register.html')
     elif request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        re_password = request.POST['re-password']
+        username = request.POST.get('username',None)
+        useremail = request.POST.get('useremail',None)
+        password = request.POST.get('password',None)
+        re_password = request.POST.get('re-password',None)
 
-        fcuser = user(
+        res_data = {}
+        if not (username and password and re_password):
+            res_data['error'] = '모든 값을 입력해야 합니다.'
+        if password != re_password:
+            #return HttpResponse('비밀번호가 다릅니다.')
+            res_data['error'] = '비밀번호가 다릅니다.'
+        else:
+            fcuser = user(
             username=username,
-            password=password
-        )
-        fcuser.save()
-        return render(request,'register.html')
+            useremail=useremail,
+            password=make_password(password)
+            )
+            fcuser.save()
+        return render(request,'register.html',res_data)
